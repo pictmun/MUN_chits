1aa5eed559d1d95d48df17864f6922809c7f3e98
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-11-16 15:54:52 +0530

    deploy

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173" || "https://mun-chits.vercel.app/",
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true, // Allow cookies
   },
--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://mun-chits.vercel.app/",
+  baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
